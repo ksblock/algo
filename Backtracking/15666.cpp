b6a72a995e78e25e191f5ce88fec4b6d41aedcc5
김시환 <ksshhses@ajou.ac.kr>
2022-05-04 12:27:31 +0900

    22.05.04

---
+++ Backtracking/15666.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void back(int st, vector<int> input, int M, int N, vector<int> ans)
+{
+    if(M == 0)
+    {
+        for(int i=0;i<ans.size();i++)
+            cout << ans[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int prev = -1;
+    for(int i=st;i<N;i++)
+    {
+        int flag = 0;
+
+        if(input[i] == prev)
+            continue;
+        
+        prev = input[i];
+        vector<int> next = ans;
+        next.push_back(input[i]);
+
+        back(i, input, M-1, N, next);
+    }
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> input(N);
+    for(int i=0;i<N;i++)
+        cin >> input[i];
+    sort(input.begin(), input.end());
+
+    vector<int> ans;
+    back(0, input, M, N, ans);
+}
